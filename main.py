c892d3c1d23d4edda1c0d6ba3c4139f48cd7a29d
Mudassir Altaf <119217471+Mudassir4545@users.noreply.github.com>
2023-05-31 02:10:13 +0500

    Update main.py

--- main.py
+++ main.py
@@ -9,25 +9,9 @@ with open("config.json","r") as c:
     params = para["params"]
 
 app = Flask(__name__)
-app.config.update(
-    MAIL_SERVER='smtp.gmail.com',
-    MAIL_PORT='465',
-    MAIL_USE_SSL='True',
-    MAIL_USERNAME =params['gmail_id'],
-    MAIL_PASSWORD =params['gmail_password']
-
+   
 )
-mail = Mail(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = params["local_server"]
-db = SQLAlchemy(app)
 
-class Contacts(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(20), nullable=False)
-    email = db.Column(db.String(20), nullable=False)
-    phone = db.Column(db.String(12), nullable=False)
-    message = db.Column(db.String(120), nullable=False)
-    date = db.Column(db.String(12), nullable=False)
 
 @app.route('/layout')
 def layout():
@@ -38,31 +22,7 @@ def portfolio_details():
 @app.route('/blog_single')
 def blog_single():
     return render_template('blog_single.html',params=params)
-@app.route('/',methods=['GET','POST'])
+@app.route('/')
 def index():
-    if (request.method == 'POST'):
-        name = request.form.get('name')
-        email = request.form.get('email')
-        phone = request.form.get('phone')
-        message = request.form.get('message')
-        print('Your Name is: ', name)
-        print('Your email is: ', email)
-        print('Your phone is: ', phone)
-        print('Your message is: ', message)
-
-        entry = Contacts(name=name, email=email, phone=phone, message=message, date=datetime.now())
-        db.session.add(entry)
-        db.session.commit()
-        mail.send_message(
-            "New Message from " + name,
-            sender=email,
-            recipients=[params['gmail_id']],
-            body=message + '\n' + phone
-        )
-
-
-    else:
-        print("ERROR........")
-
     return render_template('index.html',params=params)
 app.run(debug=True)
